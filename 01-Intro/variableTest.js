a3bc68f0ca79f9847e09744b650ee8e1c8737f73
Yashwanth Anand S <yashwanthanands@outlook.com>
2024-07-27 19:16:50 +0530

    Added variable topics intro

---
+++ 01-Intro/variableTest.js
@@ -0,0 +1,14 @@
+// string
+var greetings = 'Hello Yash';
+console.log(greetings);
+//number
+var userId = 3342.5;
+console.log(userId);
+//boolean
+var isLoggedIn = false;
+console.log(isLoggedIn);
+//type inference
+var userID = 3345.3;
+console.log(userID);
+var isLogged = false;
+console.log(isLogged);
---
+++ 01-Intro/variableTest.ts
@@ -0,0 +1,23 @@
+// string
+let greetings: string = 'Hello Yash';
+
+console.log(greetings);
+
+//number
+let userId: number = 3342.5
+
+console.log(userId);
+
+//boolean
+let isLoggedIn : boolean = false
+
+console.log(isLoggedIn);
+
+//type inference
+let userID = 3345.3
+
+console.log(userID);
+
+let isLogged = false;
+
+console.log(isLogged);
